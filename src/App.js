fa0c6379df25dc9b300023f6934aa5adbe9352c3
sahilbisht-saimanshetty <sahil.b@saimanshetty.com>
2025-04-01 19:10:16 +0530

    logo update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import LandingPage from './components/pages/LandingPage';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
